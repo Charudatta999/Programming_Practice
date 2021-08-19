4ba4ed9804460bef1cfcbc94a1be61f2213a76df
Charudatta <cjawsome999@gmail.com>
2021-08-19 15:50:57 +0530

    bubble sort program from GFG

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,7 +1,5 @@
-
-// C++ program for implementation of Bubble sort
-#include <iostream>
-using namespace std;
+// Optimized implementation of Bubble sort
+#include <stdio.h>
 
 void swap(int *xp, int *yp)
 {
@@ -10,16 +8,27 @@ void swap(int *xp, int *yp)
     *yp = temp;
 }
 
-// A function to implement bubble sort
+// An optimized version of Bubble Sort
 void bubbleSort(int arr[], int n)
 {
     int i, j;
+    bool swapped;
     for (i = 0; i < n - 1; i++)
-
-        // Last i elements are already in place
+    {
+        swapped = false;
         for (j = 0; j < n - i - 1; j++)
+        {
             if (arr[j] > arr[j + 1])
+            {
                 swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // IF no two elements were swapped by inner loop, then break
+        if (swapped == false)
+            break;
+    }
 }
 
 /* Function to print an array */
@@ -27,17 +36,17 @@ void printArray(int arr[], int size)
 {
     int i;
     for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+        printf("%d ", arr[i]);
+    printf("n");
 }
 
-// Driver code
+// Driver program to test above functions
 int main()
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
-    cout << "Sorted array: \n";
+    printf("Sorted array: \n");
     printArray(arr, n);
     return 0;
 }
