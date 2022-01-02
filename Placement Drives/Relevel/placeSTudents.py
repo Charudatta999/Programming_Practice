15ea397e154f3a94135fb6aab2ae5b64d8929952
charudatta999 <cjawsome999@gmail.com>
2022-01-02 21:47:29 +0530

    faile again

--- Placement Drives/Relevel/placeSTudents.py
+++ Placement Drives/Relevel/placeSTudents.py
@@ -23,18 +23,24 @@ for i in range(t):
         prev = "g"
     for i in range(1, len(list_of_seats)):
 
-           if boys == 0 and girls == 0:
-                break
-            if list_of_seats[i] == "#":
-                sitting[i] = "#"
-            elif prev == "g" or prev == "#" and sitting[i] == ".":
-                if boys != 0:
-                    sitting[i] = "b"
-                    boys -= 1
-            elif prev == "b" or prev == "#" and sitting[i] =="." :
-                if girls != 0:
-                    sitting[i] = "g"
-                    girls -= 1
-            prev = list_of_seats[i]
+        if boys == 0 and girls == 0:
+            break
+        if list_of_seats[i] == "#":
+            sitting[i] = "#"
+        elif prev == "#" or prev == "g" and list_of_seats[i]=="." :
+            if boys != 0:
+                sitting[i] = "b"
+                boys -= 1
+            else:
+                prev="#"
+                continue
+        elif prev == "#" or prev == "b" and list_of_seats[i]=="." :
+            if girls != 0:
+                sitting[i] = "g"
+                girls -= 1
+            else:
+                prev="#"
+                continue
+        prev = list_of_seats[i]
 
     print(sitting)
